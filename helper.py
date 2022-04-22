f51ff0bc2b83b3c7bf10b282af8f3672c4628d04
ShlackBaum <95583630+ShlackBaum@users.noreply.github.com>
2022-04-22 17:04:11 +0300

    Delete helper.py

--- helper.py
+++
@@ -1,8 +0,0 @@
-def splitter_of_strings (string):
-    string = string.split(" ")
-    string = "-".join(string)
-    string = string.split(":")
-    string = "-".join(string)
-    string = string.split(".")
-    string = "-".join(string)
-    return string
